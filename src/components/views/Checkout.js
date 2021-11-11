d06397eca1d7fd47c369eccb8731609a65e9b15e
CharlesKasasira <charleskasasira01@gmail.com>
2021-11-11 17:33:55 +0300

    working on the checkout

--- src/components/views/Checkout.js
+++ src/components/views/Checkout.js
@@ -1,17 +1,21 @@
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 import { useCart } from '../contexts/Cart'
+import Countries from '../helpers/countries.elements'
+import Districts from '../helpers/districts.element'
+import USstates from '../helpers/states.elements'
 
 function Checkout() {
-    const formRef = useRef('checkout')
+    const checkoutRef = useRef()
     const { total } = useCart()
+    const [country, setCountry] = useState('Uganda')
     
     const handlePayment = () => {}
 
     return (
         <div>
-            Checkout
+            <h1>Checkout</h1>
 
-            <form ref={"checkout"} method="post">
+            <form ref={checkoutRef} method="post">
 
                 <fieldset>
                     <legend>Billing info</legend>
@@ -23,23 +27,84 @@ function Checkout() {
 
                 <fieldset>
                     <legend>Shipping info</legend>
+                    <div>
+                        <label>Name<span  className="required-label">*</span></label>
+                        <input type="text" />
+                    </div>
+
+                    <div>
+                        <label>Company</label>
+                        <input type="text" name="" id="" />
+                    </div>
+
+                    <div>
+                        <label>Address line 1: <span className></span></label>
+                        <span className="required-label">*</span>
+                        <input type="text" name="" id="" />
+                    </div>
+
+                    <div>
+                        <label>Address line 2: <span className></span></label>
+                        <input type="text" name="" id="" />
+                    </div>
+
+                    <div>
+                        <label>Country
+                            <span className="required-label">*</span>
+                        </label>
+                        <Countries onChange={({target}) => setCountry(target.value)} required id="country" />
+                        {country === 'Uganda' 
+                        ? 
+                            <>
+                                <div>
+                                    <label>District
+                                        <span className="required-label">*</span>
+                                    </label>
+                                    <Districts id="district" />
+                                </div>
+                                <div>
+                                    <label>Town/Village</label>
+                                    <input type="text"  />
+                                </div>
+                            </>
+                        :
+                            <>
+                                <div>
+                                    <label>State
+                                    <span className="required-label">*</span>
+                                    </label>
+                                    <USstates id="us_state" />
+                                </div>
+                                <div>
+                                    <label>Town/City</label>
+                                    <input type="text" placeholder="Town/City" />
+                                </div>
+                                <div>
+                                    <label>Zip code/postal code</label>
+                                    <input type="text" placeholder="Postal code" />
+                                </div>
+                            </>
+                        
+                        }
+                    </div>
+
+
                 </fieldset>
 
-                <fieldset>
+                {/* <fieldset>
                     <legend>Payment method</legend>
                     <p>subtotal {total}</p>
                     <p>Shipping {shipping}</p>
-                    <P>Discount {discount}</P>
+                    <p>Discount {discount}</p>
                     <p>Tax {tax}</p>
                     <p>Total {total + shipping + tax - discount}</p>
-
-                </fieldset>
+                </fieldset> */}
 
                 <div>
                     <label>MoMo/MobileMoney</label>
-                    <input type="radio" name="" value="momo" ref="momo" />
+                    <input type="radio" name="payment_method" value="momo" />
                     <label>Airtel</label>
-                    <input type="radio" name="" value="airtel" ref="airtel" />
+                    <input type="radio" name="payment_method" value="airtel" />
                     <button onClick={handlePayment}>Pay now</button>
                 </div>
                 
